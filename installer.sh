cbd678ca23d0d6a635bbad69ceaab7a928b1966f
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 00:31:25 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,8 +4,9 @@
 ##
 ###########################################
 ###########################################
+#!/bin/sh
 echo
-wait
+
 wget -O /media/hd/LinuxCommands.lst https://drive.google.com/uc?id=1U84fmxU2fxjDdUkgealAouFLz0Q911Sl&export=download
 
 exit 0
