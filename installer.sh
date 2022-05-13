21493fda9d9adda11f113c114c7caf4d1cdde405
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-14 01:21:34 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,9 +5,9 @@
 #!/bin/sh
 echo
 #!/bin/sh
-
+#########################################################
 wget -O /media/hdd/LinuxCommands.lst "https://drive.google.com/uc?id=1U84fmxU2fxjDdUkgealAouFLz0Q911Sl&export=download"
-
+#########################################################
 exit 0
 #########################################################
 PACKAGE_DIR='tarekzoka/LinuxCommands/main'
